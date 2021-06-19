c6842beb2aef169e367687d7382c34b06d08c2db
h-khribich <hamza.khribich@gmail.com>
2021-06-20 00:53:50 +0200

    Ajout confirmation lors d'envoi réussi https://github.com/OpenClassrooms-Student-Center/GameOn-website-FR/issues/4

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,14 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const modalClosebtn = document.querySelector(".content > .close");
+const btnClose = document.querySelector(".close");
+const modalForm = document.querySelector(".modal-body > form");
+const modalBody = document.querySelector(".modal-body");
+const btnSubmit = document.querySelector(".modal-body .btn-submit");
+const btnGenial = modalBody.appendChild(document.createElement("button"));
+      btnGenial.style.display = "none"; // Only visible in validation message
+      btnGenial.className = "btn-submit";
+
 
 // Launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,12 +28,32 @@ function launchModal() {
   modalbg.style.display = "block";
 };
 
-// Close modal event and form
-modalClosebtn.addEventListener("click", function() {
-  modalbg.style.display = "none";
+
+// Close buttons
+[btnGenial, btnClose].forEach(function(element) {
+  element.addEventListener("click", function() {
+    modalbg.style.display = "none";
+  });
 });
 
 
+// Validation message on successful submit
+btnSubmit.addEventListener("click", function(event) {
+  event.preventDefault();
+  validate();
+});
 
 
-//validate()
+function validate() {
+  modalForm.innerText = "Merci ! Votre réservation a bien été enregistrée.";
+  modalForm.style.padding = "2rem 0";
+  btnGenial.style.display = "block";
+  btnGenial.innerText = "Génial !";
+};
+
+/*
+ADD VALIDATION MESSAGE TRANSITION, SAME AS FORM, 
+PLUS MAYBE CHANGE MODALBODY INSTEAD OF FORM ?
+
+ADD PREVENT BACKGROUND SCROLL
+*/
