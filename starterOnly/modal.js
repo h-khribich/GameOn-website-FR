18f68c16d5ec6b388dcc539cbb9c110a00ba7b7a
h-khribich <hamza.khribich@gmail.com>
2021-06-23 01:19:23 +0200

    Regex validation events names and email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,7 +9,7 @@ function editNav() {
 
 
 /*
-  --- DOM Elements ---
+  --- DOM ELEMENTS ---
 */
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -22,9 +22,14 @@ const btnSubmit = document.querySelector(".modal-body .btn-submit");
 const btnGenial = content.appendChild(document.createElement("button"));
       btnGenial.style.display = "none"; // Only visible in validation message
       btnGenial.className = "btn-submit";
+// Form DOM Elements
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const formEmail = document.getElementById("email");
+
 
 /*
-  --- Modal launch & close ---
+  --- MODAL LAUNCH & CLOSE EVENTS ---
 */
 // Launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -35,33 +40,138 @@ function launchModal() {
 };
 
 // Close buttons
-[btnGenial, btnClose].forEach(function(element) {
-  element.addEventListener("click", function() {
+[btnGenial, btnClose].forEach(function(event) {
+  event.addEventListener("click", function() {
     modalbg.style.display = "none";
   });
 });
 
 /*
-  --- Form requierements ---
+  --- FORM REQUIREMENTS ---
 */
+// First name
 
-//let isFormValid = false;
-/*
-if (array for each ?)all inputs valid,
-then isFormValid = true;
+firstName.setAttribute("placeholder", "Master");
+// Adding invalid input alert to inputs
+firstNameInvalid = document.createElement("p");
+firstName.after(firstNameInvalid);
+firstNameInvalid.innerText = "Veuilliez saisir un minimum de deux caractères";
+firstNameInvalid.style.display = "none";
 
-then
-// Validation 'load' effect
-  //setTimeout(validate, 600);
-*/
+// Last name
+lastName.setAttribute("placeholder", "Chief");
+// Adding invalid input alert to inputs
+lastNameInvalid = document.createElement("p");
+lastName.after(lastNameInvalid);
+lastNameInvalid.innerText = "Veuilliez saisir un minimum de deux caractères";
+lastNameInvalid.style.display = "none";
 
-let isFormValid = true;
-//let isFormValid = modalForm.checkValidity().value;
+// Email
+formEmail.setAttribute("placeholder", "halo@gmail.com");
+// Adding invalid input alert to inputs
+emailInvalid = document.createElement("p");
+formEmail.after(emailInvalid);
+emailInvalid.innerText = "Veuilliez saisir une adresse email valide";
+emailInvalid.style.display = "none";
+
+// Styling valid & invalid inputs
+function invalidInput(value) {
+  value.style.border = "2px solid #e54858";
+  value.nextSibling.style.display = "block";
+  value.nextSibling.style.textAlign = "right";
+  value.nextSibling.style.color = "#e54858";
+  value.nextSibling.style.fontSize = "0.8rem";
+  value.nextSibling.style.marginBottom = "-2.5rem";
+}
+
+function validInput(value) {
+  value.nextSibling.style.display = "none";
+  value.style.border = "none";
+}
+
+
+// First name validation event
+firstName.addEventListener("input", function() {
+  validFirstName(this);
+});
+
+// First name validation function
+function validFirstName(name) {
+  /* Reg exp names validation (both first and last name have similar requirements)*/
+  let regExName = new RegExp(
+    '^[a-zA-Z]{2,}$', 
+    'g' // Test all occurences
+    );
+  let testName = regExName.test(name.value);
+
+  // Personalised input event messages
+  if (testName) {
+    validInput(firstName); 
+  } else {
+    invalidInput(firstName);
+  }
+}
+
+// Last name validation event
+lastName.addEventListener("input", function() {
+  validLastName(this);
+});
+
+// Last name validation function
+function validLastName(name) {
+  /* Reg exp names validation (both first and last name have similar requirements)*/
+  let regExName = new RegExp(
+    '^[a-zA-Z]{2,}$', 
+    'g' 
+    );
+  let testName = regExName.test(name.value);
+
+  // Personalised input event messages
+  if (testName) {
+    validInput(lastName); 
+  } else {
+    invalidInput(lastName);
+  }
+}
+
+// Email validation event
+formEmail.addEventListener("input", function() {
+  validEmail(this);
+});
+
+// Last name validation function
+function validEmail(name) {
+  /* Reg exp names validation (both first and last name have similar requirements)*/
+  let regExName = new RegExp(
+    '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 
+    'g'
+    );
+  let testEmail = regExName.test(name.value);
+
+  // Personalised input event messages
+  if (testEmail) {
+    validInput(formEmail); 
+  } else {
+    invalidInput(formEmail);
+  }
+}
+
+
+
+
+
+
+
+
 
 //Disable submission button if form is invalid
-if (isFormValid === false) {
-  btnSubmit.setAttribute("disabled", "");
+if (modalForm.checkValidity() === false) {
   btnSubmit.style.backgroundColor = "gray";
+
+  //Give focus to relevant inputs
+  //modalForm.addEventListener("click", function ()
+  //document.querySelectorAll(':invalid');
+  
 }
 
 /*
@@ -70,11 +180,11 @@ if (isFormValid === false) {
 btnSubmit.addEventListener("click", function(event) {
   // Prevent form submission with page reload
   event.preventDefault();
-
-  // Validation 'load' effect
-  setTimeout(validate, 600);
+  if (modalForm.checkValidity() === true) {
+    // Validation 'load' effect
+    setTimeout(validate, 600);
+  } 
 });
-
 
 // Validating form element
 function validate() {
@@ -114,4 +224,5 @@ function validate() {
   setTimeout(validationMessage, 900); // Give form element time to leave
 };
 
-// WHEN FORM INVALID RED SENTENCES FLASH
+
+// ADD FAVICON
